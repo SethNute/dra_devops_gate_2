15a11baa2446bda9f462bc07bccdb20d5e810c31
Seth Nute <seth.nute@ibm.com>
2017-02-24 10:51:35 -0500

    Test extension uploading

--- execute.sh
+++ execute.sh
@@ -18,77 +18,21 @@
 set +e
 set +x 
 
-
-
-
-#
-# Build Grunt-Idra call
-#
-#   $1  Criteria
-#   $2  Environment
-#
-function dra_command_for_decision {
-    debugme echo -e "${no_color}"
-    node_modules_dir=`npm root`
-
-    if [ -n "$2" ] && [ "$2" != " " ]; then
-        grunt --gruntfile="$node_modules_dir/grunt-idra3/idra.js" -decision="$1" -env="$2" --no-color
-        GRUNT_RESULT=$?
-    else
-        grunt --gruntfile="$node_modules_dir/grunt-idra3/idra.js" -decision="$1" --no-color
-        GRUNT_RESULT=$?
-    fi
-
-    debugme echo "GRUNT_RESULT: $GRUNT_RESULT"
-
-    if [ $GRUNT_RESULT -ne 0 ]; then
-        exit 1
-    fi
-    
-    echo -e "${no_color}"
-}
-
-
-
-
-
-
-
-
-
-
-callOpenToolchainAPI
-
-printInitialDRAMessage
-
-installDRADependencies
-
-
-echo -e "${no_color}"
-debugme echo "DRA_CRITERIA: ${DRA_CRITERIA}"
-debugme echo "DRA_ENVIRONMENT: ${DRA_ENVIRONMENT}"
-
-debugme echo "CF_CONTROLLER: ${CF_CONTROLLER}"
-debugme echo "DRA_SERVER: ${DRA_SERVER}"
-debugme echo "DLMS_SERVER: ${DLMS_SERVER}"
-debugme echo "CF_ORG: $CF_ORG"
-debugme echo "CF_ORGANIZATION_ID: $CF_ORGANIZATION_ID"
-debugme echo "PIPELINE_INITIAL_STAGE_EXECUTION_ID: $PIPELINE_INITIAL_STAGE_EXECUTION_ID"
-debugme echo -e "${no_color}"
-
-
-
-
-if [ -n "${DRA_CRITERIA}" ] && [ "${DRA_CRITERIA}" != " " ]; then
-
-    dra_command_for_decision "${DRA_CRITERIA}" "${DRA_ENVIRONMENT}"
-    
-else
-    echo -e "${no_color}"
-    echo -e "${red}The Policy Name must be declared."
-    echo -e "${no_color}"
-fi
-
-
-
-
+cat "$TMPDIR/ibm.devops.services.pipeline.dra_devops_gate.json"
+touch "$TMPDIR/ibm.devops.services.pipeline.dra_devops_gate.json"
+echo '{"pipeline_ui": [
+            {
+              "url": "https://dra.ng.bluemix.net/report/af15c1eecc608de2dda7b22b60a48a5c",
+              "job_execution_id": '\"$TASK_ID\"',
+              "default_tab": true
+            }
+        ],
+        "report_urls": [
+            {
+                "report_url": "https://dra.ng.bluemix.net/report/af15c1eecc608de2dda7b22b60a48a5c",
+                "job_id": '\"$IDS_JOB_ID\"'
+            }
+        ]
+    }' > "$TMPDIR/ibm.devops.services.pipeline.dra_devops_gate.json"
+cat "$TMPDIR/ibm.devops.services.pipeline.dra_devops_gate.json"
+$PIPELINE_SCRIPTS_DIR/ids-set-extension-data.sh
